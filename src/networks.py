40c81bba59e29a3c2b401bef41ede11b0728755c
Jonathan Geuter <jonathan.geuter@gmx.de>
2022-11-19 23:52:42 +0100

    test

--- src/networks.py
+++ src/networks.py
@@ -75,37 +75,23 @@ class genNet(nn.Module):
         self.dim = dim
         self.length = int(math.sqrt(self.dim))
         self.l1 = nn.Sequential(
-            nn.Conv2d(2, 2, kernel_size=3, padding=1),
-            nn.ReLU(),
-            )
-        self.l2 = nn.Sequential(
-            nn.Conv2d(2, 2, kernel_size=3, padding=1),
-            nn.ReLU(),
-            )
-        self.l3 = nn.Sequential(
-            nn.Conv2d(2, 2, kernel_size=3, padding=1),
-            nn.ReLU(),
-            )
-        self.l4 = nn.Sequential(
             nn.Linear(70, 2*dim),
             nn.ReLU(),
             )
-        self.l5 = nn.Sequential(
+        self.l2 = nn.Sequential(
             nn.Linear(2*dim, 2*dim),
             nn.ReLU(),
             )
         self.layers = [
             self.l1,
-            self.l2,
-            self.l3
+            self.l2
         ]
 
     def forward(self, x):
         #x = x.reshape(x.size(0), 2, self.length, self.length)
-        #for l in self.layers:
-        #    x = l(x)
+        for l in self.layers:
+            x = l(x)
         #x = x.reshape(x.size(0), 2*self.dim)
-        x = self.l4(x)
         x = x.to(torch.float64)
         x += 1e-2
         x[:, :self.dim] /=  x[:, :self.dim].sum(1)[:, None]
