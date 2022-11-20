34b0e9d0ee032d639d21569c09ce63717366bb5e
Jonathan Geuter <jonathan.geuter@gmx.de>
2022-11-20 23:22:55 +0100

    test

--- src/DualOTComputation.py
+++ src/DualOTComputation.py
@@ -170,9 +170,9 @@ class DualApproximator:
                 x_curr, pot_curr = x[perm], pot[perm]
                 for j in range(batchsize//minibatch):
                     out = self.net(x_curr[j*minibatch:(j+1)*minibatch])
-                    #dual = compute_dual(out[:, :self.dim], out[:, self.dim:], pot_curr[j*minibatch:(j+1)*minibatch])
+                    dual = compute_dual(x_curr[j*minibatch:(j+1)*minibatch, :self.dim], x_curr[j*minibatch:(j+1)*minibatch, self.dim:], out)
                     self.optimizer.zero_grad()
-                    loss = loss_function(out, pot_curr[j*minibatch:(j+1)*minibatch]) #- 0.01*dual
+                    loss = loss_function(out, pot_curr[j*minibatch:(j+1)*minibatch]) - 0.1*dual
                     if prints:
                         print("net loss, j="+str(j)+", loss="+str(loss.item()))
                     loss.backward()
