87b625803f9a8e18fbed952c25c6f133d9c3c3f6
Jonathan Geuter <jonathan.geuter@gmx.de>
2023-01-25 23:36:34 +0100

    add bootstrap loss

--- src/DualOTComputation.py
+++ src/DualOTComputation.py
@@ -207,7 +207,8 @@ class DualApproximator:
                     pot = sinkhorn(x[:, :self.dim], x[:, self.dim:], self.costmatrix, eps, bootstrap_k, start, log=True, tens_type=torch.float32, verbose=False, min_start=1e-35, max_start=1e35)['u']
                     pot = pot - pot.sum(1)[:, None]/pot.size(1)
                     pot = pot.detach()
-                    print(pot)
+                    print(pot.min())
+                    print(pot.max())
 
             else:
                 if not max_dual:
